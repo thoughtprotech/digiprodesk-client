6aa83eda4a15860f0a487bef8305df670c430a90
YARE09 <pradyumnad092004@gmail.com>
2025-02-03 16:50:13 +0530

    Minor UI change

--- src/pages/admin/checkIns/_components/CallCard.tsx
+++ src/pages/admin/checkIns/_components/CallCard.tsx
@@ -17,8 +17,8 @@ export default function CallCard({
 }: CallCardProps) {
   return (
     <div className='w-full h-fit flex flex-col gap-2 bg-background border border-border hover:bg-highlight duration-300 p-2 rounded-md cursor-pointer' onClick={onClick}>
-      <div className="w-full flex flex-col items-center justify-between">
-        <div className="w-full flex items-start justify-between">
+      <div className="w-full flex flex-col gap-1 items-center justify-between">
+        <div className="w-full flex items-start justify-between border-b border-b-border pb-">
           <div className="flex items-center gap-1">
             <Calendar className="w-[0.7rem] text-textAlt" />
             <h1 className="text-xs text-textAlt">{date}</h1>
@@ -27,14 +27,14 @@ export default function CallCard({
             <Clock className="w-3 text-textAlt" />
             <h1 className="text-xs text-textAlt">{time}</h1>
           </div>
+          <div className="flex items-center gap-1">
+            <User className="w-3 text-textAlt" />
+            <h1 className="text-xs text-textAlt">{name}</h1>
+          </div>
         </div>
         <div className="w-full flex items-start justify-between">
           <div>
             <h1 className='font-bold text-sm'>{ticket}</h1>
-          </div>
-          <div className="flex items-center gap-1">
-            <User className="w-3 text-textAlt" />
-            <h1 className="text-xs text-textAlt">{name}</h1>
           </div>
         </div>
       </div>
