8fd83eadcef1fd3d1acec5a4c1e2ede9d66f7268
YARE0909 <pradyumnad092004@gmail.com>
2025-02-16 17:06:27 +0530

    minor change

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 const Tabs = ({ tabs }: { tabs: any }) => {
-  const [activeTab, setActiveTab] = useState(1);
+  const [activeTab, setActiveTab] = useState(0);
 
   return (
     <div className="w-full h-full mx-auto">
