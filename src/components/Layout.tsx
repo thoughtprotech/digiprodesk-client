36a7b2affc0dd048ffd41e946bc94767751688ed
YARE0909 <pradyumnad092004@gmail.com>
2025-04-09 03:17:31 +0800

    Minor change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -489,7 +489,8 @@ export default function Index({
         socket.emit("get-call-list");
         console.log("GETTING CALLS");
         socket.on("call-list-update", (data: CallQueue[]) => {
-          console.log("CALL LIST", {data});
+          console.log("CALL LIST", { callList });
+          console.log("CALL LIST DATA", { data });
           // Identify new "pending" calls
           let newPendingCalls = data.filter(
             (call) =>
